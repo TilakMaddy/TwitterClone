a1d01a6624d8b469963eb43f02cec102c0c6e508
TilakMaddy <tilak.madichetti@gmail.com>
2021-04-06 12:17:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3003;
+
+const server = app.listen(port, () => {
+  console.log("Listening on " + port);
+});
+
+app.get('/', (req, res, next) => {
+
+  res.status(200)
+    .send("Hello !");
+
+})
